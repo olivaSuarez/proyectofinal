6d5f1e9cd08caf292c4b6b188835eca9fb9d050a
olivaSuarez <olmaolsu@gmail.com>
2022-09-28 23:49:31 -0400

    cambio firebase

--- App.js
+++ App.js
@@ -6,17 +6,21 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import type {Node} from 'react';
 import {
+  Button,
   SafeAreaView,
-  ScrollView,
+  FlatList,
   StatusBar,
   StyleSheet,
   Text,
   useColorScheme,
   View,
+  Image,
 } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+import storage from '@react-native-firebase/storage';
 
 import {
   Colors,
@@ -57,40 +61,66 @@ const Section = ({children, title}): Node => {
 const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
+  const [isVisible, setIsVisible] = useState('true')
+  const [tatuajes, setTatto] = useState([])
+
+  useEffect(() => {
+    getTatto()
+  }, [])
+
+  const getTatto = () => {
+
+    firestore()
+    .collection('tatuajes')
+    .get()
+    .then(async (TatuFire)=> {
+      let tempTatto = []
+      let promesaImagen = []
+      TatuFire.forEach(TatuFire =>{
+        tempTatto.push(TatuFire.data())
+        promesaImagen.push(storage().ref(TatuFire.data().imagen).getDownloadURL())
+        console.log('tatuajes', TatuFire.data())
+      })
+      const resultPromesa = await Promise.all(promesaImagen)
+      
+      //console.log("resultado de Promesa Imagen", resultPromesa)
+      //insertado de imagen
+      resultPromesa.forEach((url, index) =>{
+        tempTatto[index].imagen = url
+      })
+
+      setTatto(tempTatto)
+    })
+
+  }
+
+  const ItemTatto = ({item}) =>{
+    console.log("item", item)
+    return(
+      <View style={styles.ContainerItem}>
+        {/* No quiere cargar la imagen */}
+        <Image source={{uri: item.imagen}} style={{width: 75, height: 75}}/> 
+        <Text style={styles.ItemTattoName}>{item.tamaño}</Text>
+        <Text style={styles.ItemTattoPrice}>{item.precio} Bs.</Text>
+      </View>
+    )
+  }
+
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView style={{flex: 1, height: '100%'}}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <FlatList
+      data={tatuajes}
+      renderItem={ItemTatto}
+      />  
+
     </SafeAreaView>
   );
 };
@@ -112,6 +142,25 @@ const styles = StyleSheet.create({
   highlight: {
     fontWeight: '700',
   },
+  ContainerItem: {
+    flexDirection: 'row',
+    padding: 10,
+    backgroundColor: 'white',
+    margin: 10,
+    elevation: 5,
+    borderRadius: 5,
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  ItemTattoName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#000'
+  },
+  ItemTattoPrice: {
+    fontWeight: 'bold',
+    fontSize: 15
+  }
 });
 
 export default App;
